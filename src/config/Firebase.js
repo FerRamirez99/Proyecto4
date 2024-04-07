4b5633a01d89b46a09e81cf37d031b129f00b4ca
FerRamirez99 <135175802+FerRamirez99@users.noreply.github.com>
2024-04-07 17:06:28 -0400

    Cambios menores

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -3,12 +3,12 @@ import { getFirestore} from "firebase/firestore"
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnWU6zsuRP4gWYNyUuPUn2uSBdVlZ-s7Y",
-  authDomain: "proyecto-4-b6746.firebaseapp.com",
-  projectId: "proyecto-4-b6746",
-  storageBucket: "proyecto-4-b6746.appspot.com",
-  messagingSenderId: "277371751391",
-  appId: "1:277371751391:web:7fe07cf24edf778c6e126a"
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId
 };
 
 // Initialize Firebase
